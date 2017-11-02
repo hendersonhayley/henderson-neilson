470123e5c913b518f6694b70fa197e26a497ba5f
Darren <Darren@10.46.16.52>
2017-11-01 22:23:38 -0600

    L08 Individual

--- YearsOfPlenty/src/cit260/yearOfPlenty/view/CropsView.java
+++ YearsOfPlenty/src/cit260/yearOfPlenty/view/CropsView.java
@@ -23,6 +23,9 @@ public class CropsView {
         
         System.out.println("\n*** Current acres available to sell: " + theCropsObject.getAcres());
         CropsView.sellLand(theCropsObject);
+        
+        System.out.println("\n*** Current bushels available to buy land: " + theCropsObject.getWheatInStore());
+        CropsView.buyLand(theCropsObject);
     }
     
     static void sellLand(Crops theCropsObject) {
@@ -48,6 +51,35 @@ public class CropsView {
             theCropsObject.setAcres(newLand);
             theCropsObject.setWheatInStore(newWheat);
             System.out.println("\nSuccess. You gained " + wheatYield + " bushels of wheat and you now have " + newLand + " acres available, as well as " + newWheat + " bushels in your stores.");
+            
+            try {
+                Thread.sleep(6000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(CropsView.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+    }
+    static void buyLand(Crops theCropsObject) {
+        Scanner keyboard = new Scanner(System.in);
+        int amount;
+        int currentLand = theCropsObject.getAcres();
+        int wheatPerAcre = theCropsObject.getHarvest();
+        int currentWheat = theCropsObject.getWheatInStore();
+        System.out.println("\nHow many acres of new land do you want to buy?");
+        
+        amount = keyboard.nextInt();
+        if (amount < 0) {
+            System.out.println("\nPlease enter a positive value.");
+            CropsView.buyLand(theCropsObject);
+        } else {
+            int newLand = currentLand + amount;
+            int wheatYield = amount / wheatPerAcre;
+            int newWheat = currentWheat - wheatYield;
+            
+            theCropsObject.setAcres(newLand);
+            theCropsObject.setWheatInStore(newWheat);
+            System.out.println("\nSuccess. You now have " + newLand + " acres of land. and you lost " + wheatYield + " bushels of wheat. You now have " + newWheat + " bushels in your stores.");
             
             try {
                 Thread.sleep(6000);
