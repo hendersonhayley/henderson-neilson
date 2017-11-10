8ca1c72225c059c1a3039262015d6ace24a5bf12
adams <adams@hayleys-macbook.local>
2017-11-10 06:25:53 -0700

    11/1/2017 hayley re adding viewInterface in.

---
+++ YearsOfPlenty/src/cit260/yearOfPlenty/view/ViewInterface.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cit260.yearOfPlenty.view;
+
+
+/**
+ *
+ * @author adams
+ */
+public interface ViewInterface {
+    public void display();
+    public int getInput();
+}
