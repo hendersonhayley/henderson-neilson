277bc412ad00ba830b809b11e8071a521246e704
Neilson <Brennan.Neilson@henryschein.com>
2017-11-16 16:46:23 -0700

    Worked on MapView

--- YearsOfPlenty/src/cit260/yearOfPlenty/view/MapView.java
+++ YearsOfPlenty/src/cit260/yearOfPlenty/view/MapView.java
@@ -5,6 +5,9 @@
  */
 package cit260.yearOfPlenty.view;
 
+import cit260.yearOfPlenty.Game;
+import yearsofplenty.YearsOfPlenty;
+
 /**
  *
  * @author adams
@@ -12,7 +15,32 @@ package cit260.yearOfPlenty.view;
 public class MapView {
     
     public static void displayMapView(){
-    System.out.println("/n*** displayMapView() function called***");
+        /*
+            game = get the currentGame from the main class
+            locations = get the 2-D locations array from the map
+            Print the title
+            Print the column numbers for each column
+            for every row in map
+            Print a row divider
+            Print the row number on a new line
+            for every column in the row
+            Print a column divider
+            location = locations[row][column]
+            if location has been visited
+            Get the mapSymbol for the scene in this location
+            Print the mapSymbol
+            else
+            Print " ?? "
+            endif
+            endFor
+            Print the ending column divider
+            endFor
+            Print the ending row divider
+        */
+    
+        YearsOfPlenty game = new YearsOfPlenty();
+        Game currentGame = game.getCurrentGame();
+        
     }
     
 }
